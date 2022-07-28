54f992ad533aa8fd4d16d75a78592c1c57728c9f
Prajjwal Gupta <54144037+PrajjwalG@users.noreply.github.com>
2022-07-28 21:38:47 +0530

    Time: 199 ms (50.00%), Space: 88 MB (25.00%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int gcd(int a, int b)
-    {
-        return b == 0 ? a : gcd(b,a%b);
-    }
+    // int gcd(int a, int b)
+    // {
+    //     return b == 0 ? a : gcd(b,a%b);
+    // }
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
         
         int g = numsDivide[0];
         sort(nums.begin(),nums.end());
         for(int i = 1; i < numsDivide.size();i++)
         {
-            g = gcd(g,numsDivide[i]);
+            g = __gcd(g,numsDivide[i]);
         }
         for(int i = 0; i < nums.size();i++)
         {
