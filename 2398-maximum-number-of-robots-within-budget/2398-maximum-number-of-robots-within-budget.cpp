5957609d08597c0858f68a40b23b4064828fcd5f
Prajjwal Gupta <54144037+PrajjwalG@users.noreply.github.com>
2022-09-09 20:33:35 +0530

    Time: 909 ms (27.56%), Space: 149.6 MB (23.01%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    typedef long long ll;
+    int maximumRobots(vector<int>& ct, vector<int>& rc, long long budget) 
+	{
+        multiset<int> ms;
+        ll start=0,end=0,ans=0,n=ct.size(),s=0;
+        while(end<n)
+		{
+            s+=rc[end];
+            ms.insert(ct[end++]);
+            ll val = (*ms.rbegin())+(end-start)*s;
+            while(val>budget)
+			{
+                s-=rc[start];
+                ms.erase(ms.find(ct[start++]));
+                val =ms.empty()?0 : (*ms.rbegin())+(end-start)*s;
+            }
+            ans=max(ans,end-start);
+        }
+        return ans;
+    }
+};
